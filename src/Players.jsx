562231ff9e43ef83da79e43ef15a4a11cc393aac
YOUNGSUCIOGUAPO <oweledavid@ymail.com>
2023-07-05 00:12:23 +0100

    initail commit for checkpoint

---
+++ src/Players.jsx
@@ -0,0 +1,37 @@
+const players = [
+  {
+    name: "Lionel Messi",
+    team: "Inter Miami",
+    nationality: "Argentina",
+    jerseyNumber: 30,
+    age: 34,
+    imageURL: "https://idsb.tmgrup.com.tr/ly/uploads/images/2022/12/19/thumbs/800x531/247181.jpg?v=1671435583",
+  },
+  {
+    name: "Ronaldinho",
+    team: "Inter Milan",
+    nationality: "Brazil",
+    jerseyNumber: 10,
+    age: 42,
+    imageURL: "https://media.gettyimages.com/id/98784183/photo/ac-milan-v-acf-fiorentina-serie-a.jpg?s=1024x1024&w=gi&k=20&c=dW82I20sSq1vE0Pnj0LNdUIFK8qGBTJG8Yz07S9vTOk=",
+  },
+  {
+    name: "Ronaldo",
+    team: "Al-Nassar",
+    nationality: "Portugal",
+    jerseyNumber: 9,
+    age: 45,
+    imageURL: "https://media.gettyimages.com/id/1325105287/photo/cristiano-ronaldo-of-portugal-celebrates-after-scoring-their-sides-first-goal-during-the-uefa.jpg?s=612x612&w=0&k=20&c=IzLSVRLIX56Bm3YsCs60AQ0Yi0mjFU0Qy28FqdKAaTE=",
+  },
+  {
+    name: "Andrea Pirlo",
+    team: "Juventus",
+    nationality: "Italy",
+    jerseyNumber: 21,
+    age: 42,
+    imageURL: "https://e2.365dm.com/13/01/800x600/Andrea-Pirlo-Juventus-Champions-League_2890951.jpg?20130622135601",
+  }
+];
+
+
+export default players;
